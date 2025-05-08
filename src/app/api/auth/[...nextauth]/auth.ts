db954a4d3bb7511813b06179ce13014fd88f912d
Valeria Popova <Oleg-twice>
2025-05-08 23:41:26 +0300

    fix

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import { NextAuthOptions } from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 
 export const authOptions: NextAuthOptions  = {
--- src/app/api/place-order/route.ts
+++ src/app/api/place-order/route.ts
@@ -1,7 +1,7 @@
 // app/api/place-order/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../auth/[...nextauth]/auth";
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession({ req, ...authOptions });
